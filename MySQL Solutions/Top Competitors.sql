f939b27415b9c71ec3544953470bdd5dac2f9fa6
akscoder082003 <ashishk.singh1008@gmail.com>
2023-07-03 17:31:02 +0530

    solved

--- MySQL Solutions/Top Competitors.sql
+++ MySQL Solutions/Top Competitors.sql
@@ -1,4 +1,7 @@
--- Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. Order your output in descending order by the total number of challenges in which the hacker earned a full score. If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
+-- Julia just finished conducting a coding contest, and she needs your help assembling the leaderboard! 
+-- Write a query to print the respective hacker_id and name of hackers who achieved full scores for more than one challenge. 
+-- Order your output in descending order by the total number of challenges in which the hacker earned a full score. 
+-- If more than one hacker received full scores in same number of challenges, then sort them by ascending hacker_id.
 
 select hackers.hacker_id, hackers.name from hackers
 join submissions on hackers.hacker_id = submissions.hacker_id
