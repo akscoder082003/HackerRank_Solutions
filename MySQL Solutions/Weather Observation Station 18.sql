c936d02072afe881ad4b0890337e5baf16338065
akscoder082003 <ashishk.singh1008@gmail.com>
2023-12-31 13:59:15 +0530

    solved

---
+++ MySQL Solutions/Weather Observation Station 18.sql
@@ -0,0 +1,12 @@
+-- Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+
+-- - a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+-- - b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+-- - c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+-- - d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+-- Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+
+-- Manhattan Distance = (a-c) + (b-d) 
+
+select round(abs(min(LAT_N) - max(LAT_N)) + 
+abs(min(LONG_W) - max(LONG_W)), 4) from STATION;
