e2f4553404759cb2925cdac9a3108e61e5cc3d56
akscoder082003 <ashishk.singh1008@gmail.com>
2023-12-31 14:08:16 +0530

    updated

--- MySQL Solutions/Weather Observation Station 18.sql
+++ MySQL Solutions/Weather Observation Station 18.sql
@@ -4,7 +4,7 @@
 -- - b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
 -- - c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
 -- - d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
--- Query the Manhattan Distance between points  and  and round it to a scale of  decimal places.
+-- Query the Manhattan Distance between points P1 and P2 and round it to a scale of 4 decimal places.
 
 -- Manhattan Distance = (a-c) + (b-d) 
 
