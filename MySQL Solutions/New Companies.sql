18fb9323c5dbfe73adde6f4ca0f09765210286f1
akscoder082003 <ashishk.singh1008@gmail.com>
2023-12-31 12:26:38 +0530

    updated

--- MySQL Solutions/New Companies.sql
+++ MySQL Solutions/New Companies.sql
@@ -1,11 +1,8 @@
-
-SELECT
-Company.company_code,Company.founder,
-COUNT(distinct Employee.lead_manager_code),
-COUNT(distinct Employee.senior_manager_code),
-COUNT(distinct Employee.manager_code),
-COUNT(distinct Employee.employee_code) 
-FROM Company JOIN Employee ON 
-Company.company_code=Employee.company_code 
-group by Company.company_code,
-Company.founder;
+select Company.company_code, Company.founder,
+count(distinct Employee.lead_manager_code),
+count(distinct Employee.senior_manager_code),
+count(distinct Employee.manager_code),
+count(distinct Employee.employee_code)
+from Company join Employee on 
+Company.company_code = Employee.company_code
+group by Company.company_code, Company.founder;
