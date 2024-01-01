ea2513676879cf23fd8f4d121d374a798a32a546
akscoder082003 <ashishk.singh1008@gmail.com>
2024-01-01 21:18:44 +0530

    solved

--- MySQL Solutions/Weather Observation Station 20.sql
+++ MySQL Solutions/Weather Observation Station 20.sql
@@ -1,3 +1,4 @@
 -- A median is defined as a number separating the higher half of a data set from the lower half. 
 -- Query the median of the Northern Latitudes (LAT_N) from STATION and round your answer to 4 decimal places.
 
+select round(LAT_N, 4) from STATION order by LAT_N limit 1 offset 249
