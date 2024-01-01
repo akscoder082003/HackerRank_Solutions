9136770922388c7660289d474b6d9bbb074d3c0b
akscoder082003 <ashishk.singh1008@gmail.com>
2024-01-01 12:45:31 +0530

    solved

--- MySQL Solutions/Weather Observation Station 19.sql
+++ MySQL Solutions/Weather Observation Station 19.sql
@@ -1,10 +1,18 @@
 -- Consider P1(a,c) and P2(b,d) to be two points on a 2D plane where (a,b) 
 -- are the respective minimum and maximum values of Northern Latitude (LAT_N) 
 -- and (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+-- P1(a,c),
+-- P2(b,d)
 
+-- a = min(LAT_N) 
+-- b = max(LAT_N) 
+-- c = min(LONG_W) 
+-- d = max(LONG_W) 
 -- Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits.
 
--- Euclidean distance = √(a - c)² + (b - d)²
+-- Euclidean distance = √(b - a)² + (d - c)²   
 
-select round(abs(min(LAT_N) - max(LAT_N)) + 
-abs(min(LONG_W) - max(LONG_W)), 4) from STATION;
+select 
+round(sqrt(pow(max(LAT_N) - min(LAT_N), 2) + 
+    pow(max(LONG_W) - min(LONG_W), 2)), 4) 
+from STATION;
